8f32c2c48712eeb9b914ec1cf219cdced723338b
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-04 12:53:16 +0000

    Add icons to themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import { Switch, Theme } from "@radix-ui/themes";
+import { Box, Flex, Switch, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { type AppType } from "next/app";
 import Head from "next/head";
 import { useState } from "react";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -22,12 +23,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           <meta name="description" content="App containing useful utils" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Switch
-          checked={isDarkMode}
-          onCheckedChange={() => {
-            setIsDarkMode(!isDarkMode);
-          }}
-        />
+        <Box p="2">
+          <Flex gap={"2"} align={"center"}>
+            <SunIcon />
+            <Switch
+              checked={isDarkMode}
+              onCheckedChange={() => {
+                setIsDarkMode(!isDarkMode);
+              }}
+            />
+            <MoonIcon />
+          </Flex>
+        </Box>
         <Component {...pageProps} />
       </Theme>
     </ClerkProvider>
