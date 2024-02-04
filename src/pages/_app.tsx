768bc29ccd828b512dc540c044210121ee6bec6d
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-04 11:32:00 +0000

    Dark mode toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { Theme } from "@radix-ui/themes";
+import { Switch, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { type AppType } from "next/app";
 import Head from "next/head";
+import { useState } from "react";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
   return (
     <ClerkProvider {...pageProps}>
-      <Theme
-        accentColor="blue"
-        grayColor="mauve"
-        radius="large"
-        appearance="dark"
-      >
+      <Theme appearance={isDarkMode ? "dark" : "light"}>
         <Head>
           <title>My Utils</title>
           <meta name="description" content="App containing useful utils" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <Switch
+          checked={isDarkMode}
+          onCheckedChange={() => {
+            setIsDarkMode(!isDarkMode);
+          }}
+        />
         <Component {...pageProps} />
       </Theme>
     </ClerkProvider>
