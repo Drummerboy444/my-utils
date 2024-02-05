2693e81634c808716b312bfdf95e89d758993bf1
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2024-02-05 20:12:18 +0000

    Simplify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,12 @@
-import { Flex, Section, Text } from "@radix-ui/themes";
+import { Flex, Text } from "@radix-ui/themes";
 import { Page } from "~/components/Page";
 
 export default function Custom404Page() {
   return (
     <Page>
-      <Section>
-        <Flex justify="center">
-          <Text size="9">404</Text>
-        </Flex>
-      </Section>
+      <Flex justify="center">
+        <Text size="9">404</Text>
+      </Flex>
     </Page>
   );
 }
