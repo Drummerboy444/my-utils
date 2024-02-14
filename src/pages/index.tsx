fcad30d8603c1ed1ad38fb6e57c08d594d41452f
Anthony Murphy <anthonymurphy252@gmail.com>
2024-02-14 00:16:45 +0000

    Tweak homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { HomeIcon } from "@radix-ui/react-icons";
-import { Flex, Text } from "@radix-ui/themes";
 import { Page } from "~/components/Pages/Page";
 
 export default function HomePage() {
-  return (
-    <Page>
-      <Flex align="center" gap="2">
-        <HomeIcon />
-        <Text>This is the home page...</Text>
-      </Flex>
-    </Page>
-  );
+  return <Page header="Home page" subheader="There's not much here yet..." />;
 }
